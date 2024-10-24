d4ae116a8961efca3dfa0793cc04cf2c5bb279e9
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2024-10-24 11:07:13 +0500

    add

---
+++ main.py
@@ -0,0 +1,534 @@
+# 1 - task
+# print("Hello World")
+
+# 2 - task
+# x = input("Salom")
+# print(x)
+# x = input("Yangi habar")
+# print(x)
+
+# 3 - task
+# my_var = "Python"
+# print(my_var)
+
+# 4 - task
+# a ,b, c = 5 ,10, 15
+# print(a, b, c)
+
+# 5 - task
+# x = y = z = "olma"
+# print(x, y , z)
+
+# 6 - task
+# a = 10
+# print(type(a))
+
+# 7 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# print(mevalar)
+
+# 8 - task
+# a = 5
+# a = a + 10
+# print(a)
+
+# 9 - task
+# a = 3
+# b = 4
+# print(a + b)
+
+# 10 - task
+# x = 'Python'
+# def changeX():
+#     global x  
+#     x = "Dasturlash" 
+# changeX()
+# print(x) 
+
+# 11 - task
+# ism = "Ali"
+# familya = "Valiyev"
+# print(ism , familya)
+
+# 12 - task
+# a = 3.5
+# b = 2.5
+# print(a + b)
+
+# 13 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# print(mevalar[1])
+
+# 15 - task
+# a = 10
+# a += 5
+# print(a)
+
+# 16 - task
+# age = input("Yosh Kirit: ")
+# if int(age) >= 18 :
+#     print("Kattalar")
+# else:
+#     print("Kichiklar")
+
+# 17 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# mevalar.append("nok")
+# print(mevalar)
+
+# 18 - task
+# a = 1
+# b = 7
+# if a > b:
+#     print(a , "Katta" , b , "dan")
+# else : 
+#     print(b , "Katta" , a , "dan")
+
+
+# 19 - task
+# x, y, z = "A", "B", "C"
+# print(x,y,z)
+
+# 20 - task
+# matn = 'salom'
+# print(len(matn))
+
+# 21 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# mevalar.remove("banan")
+# print(mevalar)
+
+# 22 - task
+# def textPrint():
+#     print("Salom!")
+# textPrint()
+
+# 23 - task
+# raqam = 100
+# print(str(raqam))
+
+# 24 - task
+# a = 'Salom'
+# def achange():
+#     a = "Men localman"
+# print(a)
+
+# 25 - task
+# yangi_ozgarituvchi = None
+# print(yangi_ozgarituvchi)
+
+# 26 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# for x in mevalar:
+#     print(x)
+
+# 27 - task
+# ism = input("Ism Kirit: ")
+# print("Salom" , ism)
+
+# 28 - task
+# a = 5
+# b = 10
+# print(2 * (a + b))
+
+# 29 - task
+# mevalar = ["olma", "banan", "apelsin"]
+# meva_name = input("Meva Nomini kirit: ")
+# for x in mevalar:
+#     if x == meva_name:
+#         print(x, "bor")
+
+# 30 - task
+# age = input("Yosh Kirit: ")
+# if int(age) >= 20 :
+#     print("Kattalar")
+# else:
+#     print("Kichiklar")
+
+# 31 - task
+# matn = "HELLO WORLD"
+# print(matn.lower())
+
+# 32 - task
+# disc = {
+#     "name": "Mukhammad",
+#     "age": 20
+# }
+# disc.clear()
+# print(disc)
+
+# 33 - task
+# disc = {
+#     "name": "Mukhammad",
+#     "age": 20
+# }
+# cloneDisc = disc.copy()
+# print(cloneDisc)
+
+# 34 - task
+# kalitlar = ['a','b','c','d','e','f']
+# new_disc = dict.fromkeys(kalitlar , 0)
+# print(new_disc)
+
+# 35 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# a = 'r'
+# def searchKey():
+#     if a in disc:  
+#         print(disc[a])
+#     else:
+#         print("Not Found")
+# searchKey()
+
+# 36 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# value = disc.items()
+# print(value)
+
+# 37 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# value = list(disc)
+# print(value)
+
+# 38 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# deleteValue = disc.pop('a')
+# print(disc , deleteValue)
+
+# 39 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# print(disc.popitem())
+
+# 40 - task
+# key = 'v'
+# value = 3
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# disc.setdefault(key , value)
+# print(disc)
+
+# 41 - task
+# disc1 = {'a': 1, 'b': 2, 'c': 3}
+# disc2 = {'b': 20, 'd': 4, 'e': 5}
+# disc1.update(disc2)
+# print(disc1)
+
+# 42 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# print(list(disc.values()))
+
+# 43 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# discCopy = disc.copy()
+# disc.setdefault('v', 5)
+# print(discCopy)
+# print(disc)
+
+# 44 - task
+# keys = ['a','b','c','d','e','f']
+# disc = dict.fromkeys(keys, None)
+# print(disc)
+
+# 45 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# disc.clear()
+# print(disc)
+
+# 46 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def get_value(key, default=None):
+#     return disc.get(key, default)
+# print(get_value('d', 10))     
+
+# 47 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def getKey(key):
+#     if key in disc:
+#         return(key, disc[key])
+#     else:
+#         return "Yoq"
+# print(getKey('a'))
+
+# 48 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def allKey() :
+#     return disc.keys()
+# print(allKey())
+
+# 49 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def updateDisc(value):
+#     disc.update(value)
+#     return disc 
+# new_disc = {'a': 22, 'b': 22} 
+# updated_dict = updateDisc(new_disc)  
+# print(updated_dict)
+
+# 50 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def deleteDisc(value):
+#     return disc.pop(value)
+# print(deleteDisc('c'))
+
+# 51 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def deleteDisc():
+#     return disc.popitem()
+# print(deleteDisc())
+
+# 52 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def createDisk(key):
+#     if key in disc:  
+#         disc[key]
+#         return disc
+#     else:
+#         disc[key] = 5  
+#         return disc
+# key = 's'
+# print(createDisk(key))
+
+# 53 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def allValues(disc):
+#     return disc.values()
+# print(allValues(disc))
+
+# 54 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# def updateElment(new_disc):
+#     disc.update(new_disc)
+#     return disc
+# new_disc = {'a': 99, 'b': 999, 'c': 9999}
+# print(updateElment(new_disc))
+
+# 55 - task
+# new_disc = dict.fromkeys('a', 5)
+# print(new_disc)
+
+# 56 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# disc.clear()
+# print(disc)
+
+# 57 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# keyValue = disc.get('a')
+# print(keyValue)
+
+# 58 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# value = disc.get('s')
+# if value is None:
+#     disc['s'] = 7  
+# print(disc)
+
+# 59 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# print(disc.values())
+
+# 60 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# print(disc.items())
+
+# 61 - task
+# disc = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+# new_disc = {'a': 12, 'b': 22}
+# disc.update(new_disc)
+# print(disc)
+
+# 62 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.append(9)
+# print(nums)
+
+# 63 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.clear()
+# print(nums)
+
+# 64 - task
+# nums = [1,2,3,4,5,6,7,8]
+# new_nums = nums.copy()
+# print(new_nums)
+
+# 65 - task
+# nums = [1,2,3,4,5,6,7,8,8]
+# print(nums.count(8)) 
+
+# 66 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums2 = [4,5,6,7,8,9]
+# nums.extend(nums2)
+# print(nums)
+
+# 67 - task
+# nums = [1,2,3,4,5,6,7,8]
+# print(nums.index(3))
+
+# 68 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.insert(3, 5)
+# print(nums)
+
+# 69 - task
+# nums = [1,2,3,4,5,6,7,8]
+# print(nums.pop(2))
+# print(nums)
+
+# 70 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.remove(3)
+# print(nums)
+
+# 71 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.reverse()
+# print(nums)
+
+# 72 - task
+# nums = [1,2,3,4,5,6,7,8,2,4,7]
+# nums.sort()
+# print(nums)
+
+# 73 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.append(2)
+# nums.sort()
+# print(nums)
+
+# 74 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums.clear()
+# new_value = [1,2,3,5]
+# nums.append(new_value)
+# print(nums)
+
+# 75 - task
+# nums = [1,2,3,4,5,6,7,8]
+# nums2 = [3,5,7,8]
+# numsCopy = nums.copy()
+# nums2.extend(numsCopy)
+# print(nums2)
+
+# 76 - task
+# nums = [1,2,3,4,5,6,7,8,8]
+# value = 9
+# if value in nums: 
+#     print(nums.count(value))
+
+# 77 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# value = 3
+# if value in nums:
+#     print(nums.index(value))
+# else:
+#     print("Xato")
+
+# 78 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums.insert(0,4)
+# print(nums)
+
+# 79 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# print(nums.pop())
+# print(nums)
+
+# 80 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums.remove(8)
+# print(nums)
+
+# 81 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums.append(9)
+# nums.sort()
+# nums.reverse()
+# print(nums)
+
+# 82 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums2 = [3,5,7,8]
+# nums.clear()
+# nums.extend(nums2)
+# nums.append(99)
+# print(nums)
+
+# 83 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums2 = [3,5,7,8]
+# copynums = nums.copy()
+# copynums.sort()
+# copynums.reverse()
+# print(copynums)
+
+# 84 - task
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+# nums.sort()
+# nums.clear()
+# nums.append({1,2,3,4,5})
+# print(nums)
+
+# 85 - task
+# my_set = {1,2,3,4,5,6}
+# my_set.add(7)
+# print(my_set)
+
+# 86 - task
+# my_set = {1,2,3,4,5,6}
+# my_set.clear()
+# print(my_set)
+
+# 87 - task
+# my_set = {1,2,3,4,5,6}
+# my_set_copy = my_set.copy()
+# print(my_set_copy)
+
+# 88 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {4,2,3,4,9,7}
+# print(my_set.difference(my_set2))
+
+# 89 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {4,2,3,4,9,7}
+# my_set.difference_update(my_set2)
+# print(my_set)
+
+# 90 - task
+# my_set = {1,2,3,4,5,6}
+# my_set.discard(3)
+# print(my_set)
+
+# 91 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {4,2,3,4,9,7}
+# my_set.intersection(my_set2)
+# print(my_set.intersection(my_set2))
+
+# 92 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {4,2,3,4,9,7}
+# my_set2.intersection_update(my_set)
+# print(my_set.intersection(my_set2))
+
+# 93 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {7,8,9}
+# print(my_set.isdisjoint(my_set2))
+
+# 94 - task
+# my_set = {1,2,3,4,5,6}
+# my_set2 = {1,2,3,4,5,6}
+# print(my_set.issubset(my_set2))
+
+# 95 - task
+# my_set = {1,2,3,4,5,6}
+# value = my_set.pop()
+# print(value)
+# print(my_set)
+
+# 96 - task
+# my_set = {1,2,3,4,5,6}
+# my_set.remove(2)
+# print(my_set)
